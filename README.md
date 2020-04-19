86f5781857b1ebaf1221818bf15be1534852a7c7
Rhemaike <rhemaike@gmail.com>
2020-04-18 22:05:15 -0500

    in the process of making code to extract song features from tracks from playlist

---
+++ README.md
@@ -0,0 +1,3 @@
+### Before you start code input this 2 commands if using windows
+set SPOTIPY_CLIENT_ID=658eab07309b4d11846fa3636413a425
+set SPOTIPY_CLIENT_SERCRET=6b1d14ccfccc4c1bb74d08542337ce1f
--- song_import.py
+++ song_import.py
@@ -72,40 +72,96 @@ if __name__ == "__main__":
     else:
         name = 'Radiohead'
 
-    # results = spotify.search(q='artist:' + name, type='artist')
-    # items = results['artists']['items']
-    # if len(items) > 0:
-    #     artist = items[0]
-        # print(artist['name'], artist['images'][0]['url'])
-        # print(artist)
-
-    ## STUFF NOW IN GET SONG KEY USED TO BE HERE
-
-
-
     # ================== Start creating Sond dataframe =====================
-    songdf = pd.DataFrame(columns=["song_title", "tempo", "key", "Genre"])
-    # print(songdf)
-    songxl_name = "Billboard_songs_2010-19.xlsx"
-    sng = Song(songxl_name)
-    sngnames = sng.get_names()
+    # songdf = pd.DataFrame(columns=["song_title", "tempo", "key", "Genre"])
+    # songxl_name = "Billboard_songs_2010-19.xlsx"
+    # sng = Song(songxl_name)
+    # sngnames = sng.get_names()
 
-    songdf = pd.DataFrame(columns=["song_title", "key", "artist","tempo"])
+    # songdf = pd.DataFrame(columns=["song_title", "key", "artist","tempo"])
 
-    for name in sngnames:
-        # print("name is", name)
-        # print("Song:",name)
-        key, artist, tempo = get_key(name)
-        info = {"song_title":name,
-            "key":key,
-            "tempo":tempo,
-            "artist":artist
-            }
-        songdf = songdf.append(info,ignore_index=True)
-        
-    # name = "shake it out"
-    # spotify = get_key(name)
-    songdf.to_excel("output.xlsx") 
+    # for name in sngnames:
+    #     # print("name is", name)
+    #     # print("Song:",name)
+    #     key, artist, tempo = get_key(name)
+    #     info = {"song_title":name,
+    #         "key":key,
+    #         "tempo":tempo,
+    #         "artist":artist
+    #         }
+    #     songdf = songdf.append(info,ignore_index=True)
+
+    # songdf.to_excel("output.xlsx") 
+
+    playlist_name = "Neural Beats - Study and Focus"
+    # playlist = spotify.search(q="playlist:"+playlist_name,type="playlist")
+    # name = "creep"
+    # song = spotify.search(q="track:"+name, type='track') 
+    # print(playlist)
+    # print(song)
+    cooljams = "1dku1CDmjYpuXW9m2Zs7ni"
+    NN = "7pYSJlHb5THXjtZ1JjHNXX"
+    test = "71UwSzImcFePNOHMAh3v4h"
+    coolvibes= "5M6G2d6F4LIobYnh8Gcjpp"
+    playlist = spotify.playlist(coolvibes)
+
+    # print(playlist)
+
+    # for key,item in playlist.items():
+    #     print(key)
+    #     if key=="tracks":
+    #         for key2,item2 in item.items():
+    #             print(key2)
+    #             if key2 == "items":
+    #                 print("---------------------------------------------")
+    #                 for stuff in item2:
+    #                     for key3,item3 in stuff.items():
+    #                         print("**********************")
+    #                         print(key3)
+    #                         if key3 == "track":
+    #                             print("+++++++++++++++++++++")
+    #                             for key4,item4 in item3.items():
+    #                                 print(key4)
+    #                                 if key4 == "name":
+    #                                     print("NAME IS {}".format(item4))
+    #                             print("+++++++++++++++++++++")
+    #                         print("**********************")
+    #                         # print("---------------------------------------------")
+    #                         # for key4,item4 in item3.items():
+    #                         #     print(key4)
+    #                         # print("---------------------------------------------")
+
+    #                 # print(item2)
+    #                 print("There are {} tracks".format(len(item2))) 
+    #                 # print(item2[0])
+                        
+
+    #                 print("---------------------------------------------")
+    #         print()
+
+    items = playlist["tracks"]["items"]
+    for item in items:
+        print("Name is song: {}".format(item["track"]["name"]))
+        print("id is: {}".format(item["track"]["id"]))
+        song_id = item["track"]["id"]
+
+    song = spotify.track(song_id)
+    # print(song)
+    aud = spotify.audio_analysis(song_id)
+    track = aud['track']
+    ft = spotify.audio_features(song_id)
+    f1 = ft[0]
+    # print(track)
+    features = ["key",  "mode",  "time_signature", "acousticness", "danceability", "energy", 
+    "instrumentalness", "liveness","loudness", "speechiness", "valence", "tempo"]
+    for key, item in f1.items():
+        print(key)
+    for feature in features:
+        print(f1[feature])
+
+    # tracks = spotify.playlist_tracks(NN)
+    # print(len(tracks["items"]))
+
         
 
 
